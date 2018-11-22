fb50db06286bc2bc328faf54598278d226241abb
rogthefrog <rogthefrog@gmail.com>
2018-11-21 18:39:42 -0800

    added selinux related packages; formatting

--- ansible/bootstrap.sh
+++ ansible/bootstrap.sh
@@ -3,7 +3,14 @@
 
 sudo dnf update -y
 
-sudo dnf install -y git python3 python3-dnf dnf-plugins-core
+sudo dnf install -y \
+    dnf-plugins-core \
+    git \
+    libselinux-python \
+    libselinux-python3 \
+    python3 \
+    python3-dnf
+
 
 rm -rf ./venv
 
